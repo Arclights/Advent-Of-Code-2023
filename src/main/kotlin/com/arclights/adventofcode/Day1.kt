f733a3f7bb264921732d44b3e9f045358dfd8b94
Tommy <tommy.kvant@gmail.com>
2023-12-01 21:08:34 +0100

    Day 1 part 2

--- src/main/kotlin/com/arclights/adventofcode/Day1.kt
+++ src/main/kotlin/com/arclights/adventofcode/Day1.kt
@@ -2,9 +2,19 @@ package com.arclights.adventofcode
 
 fun main() {
     part1()
+    part2()
 }
 
 fun part1() {
+    fun findFirstNumber(line: String) = line.dropWhile { it.isDigit().not() }.take(1).toInt()
+    fun findLastNumber(line: String) = line.dropLastWhile { it.isDigit().not() }.takeLast(1).toInt()
+
+    fun extractCalibrationValue(line: String): Int {
+        val firstNumber = findFirstNumber(line)
+        val lastNumber = findLastNumber(line)
+        return combineNumbers(firstNumber, lastNumber)
+    }
+
     val input = read("Day1.txt")
     val sum = input
         .map(::extractCalibrationValue)
@@ -12,12 +22,42 @@ fun part1() {
     println(sum)
 }
 
-fun extractCalibrationValue(line: String): Int {
-    val firstNumber = findFirstNumber(line)
-    val lastNumber = findLastNumber(line)
-    return combineNumbers(firstNumber, lastNumber)
+fun part2() {
+    fun findNumber(
+        line: String,
+        wordComparer: (String, String) -> Boolean,
+        charPicker: (String) -> Char,
+        lineMutator: (String) -> String
+    ): Int = when {
+        wordComparer(line, "one") -> 1
+        wordComparer(line, "one") -> 1
+        wordComparer(line, "two") -> 2
+        wordComparer(line, "three") -> 3
+        wordComparer(line, "four") -> 4
+        wordComparer(line, "five") -> 5
+        wordComparer(line, "six") -> 6
+        wordComparer(line, "seven") -> 7
+        wordComparer(line, "eight") -> 8
+        wordComparer(line, "nine") -> 9
+        charPicker(line).isDigit() -> charPicker(line).digitToInt()
+        else -> findNumber(lineMutator(line), wordComparer, charPicker, lineMutator)
+    }
+
+    fun findFirstNumber(line: String): Int = findNumber(line, String::startsWith, String::first) { it.drop(1) }
+
+    fun findLastNumber(line: String): Int = findNumber(line, String::endsWith, String::last) { it.dropLast(1) }
+
+    fun extractCalibrationValue(line: String): Int {
+        val firstNumber = findFirstNumber(line)
+        val lastNumber = findLastNumber(line)
+        return combineNumbers(firstNumber, lastNumber)
+    }
+
+    val input = read("Day1.txt")
+    val sum = input
+        .map(::extractCalibrationValue)
+        .sum()
+    println(sum)
 }
 
 fun combineNumbers(number1: Int, number2: Int) = number1 * 10 + number2
-fun findFirstNumber(line: String) = line.dropWhile { it.isDigit().not() }.take(1).toInt()
-fun findLastNumber(line: String) = line.dropLastWhile { it.isDigit().not() }.takeLast(1).toInt()
