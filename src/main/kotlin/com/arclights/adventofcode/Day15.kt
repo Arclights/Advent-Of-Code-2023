1e72cb7d8a002de29063371fb0a1da6f1dae4b1d
Tommy <tommy.kvant@gmail.com>
2023-12-25 14:06:39 +0100

    Day 15 part 2

--- src/main/kotlin/com/arclights/adventofcode/Day15.kt
+++ src/main/kotlin/com/arclights/adventofcode/Day15.kt
@@ -1,9 +1,11 @@
 package com.arclights.adventofcode
 
 import com.arclights.adventofcode.Day15.part1
+import com.arclights.adventofcode.Day15.part2
 
 fun main() {
     part1()
+    part2()
 }
 
 object Day15 {
@@ -13,7 +15,66 @@ object Day15 {
         println(result)
     }
 
-    private fun hash(input:String) = input.fold(0){acc, c -> ((acc + c.code) * 17) % 256 }
+    fun part2() {
+        val input = parseSequence("Day15.txt")
+        val boxes = input.fold(Boxes()) { boxes, operation ->
+            when (operation) {
+                is AddOperation -> boxes.add(operation.lens)
+                is RemoveOperation -> boxes.remove(operation.label)
+                else -> throw IllegalArgumentException("Unknown operation: $operation")
+            }
+        }
+        println(boxes.focusingPower())
+    }
 
-    private fun parse(file:String) = read(file)[0].split(",")
+    private fun hash(input: String) = input.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
+
+    private class Boxes {
+        var boxes = mutableMapOf<Int, List<Lens>>()
+
+        fun add(lens: Lens): Boxes =
+            boxes.getOrDefault(hash(lens.label), listOf())
+                .let { lenses ->
+                    boxes[hash(lens.label)] = if (lenses.any { it.label == lens.label }) {
+                        lenses.map { if (it.label == lens.label) lens else it }
+                    } else {
+                        lenses.plus(lens)
+                    }
+                }
+                .let { this }
+
+        fun remove(label: String): Boxes =
+            boxes.getOrDefault(hash(label), listOf())
+                .let { lenses -> boxes[hash(label)] = lenses.filter { it.label != label } }
+                .let { this }
+
+        fun focusingPower() = boxes.entries
+            .map { (boxNbr, lenses) -> lenses.mapIndexed { slot, lens -> (boxNbr + 1) * (slot + 1) * lens.focalLength } }
+            .flatten()
+            .sum()
+
+        override fun toString(): String {
+            return boxes.toString()
+        }
+    }
+
+    private fun parseSequence(file: String) = parse(file).map { step ->
+        when {
+            step.contains("=") -> step
+                .split("=")
+                .let { (label, valueString) -> AddOperation(Lens(label, valueString.toInt())) }
+
+            step.endsWith("-") -> RemoveOperation(step.dropLast(1))
+
+            else -> throw IllegalStateException("Unknown step: $step")
+        }
+    }
+
+    private fun parse(file: String) = read(file)[0].split(",")
+
+    private data class Lens(val label: String, val focalLength: Int)
+
+    private data class AddOperation(val lens: Lens) : Operation
+    private data class RemoveOperation(val label: String) : Operation
+    private interface Operation
 }
