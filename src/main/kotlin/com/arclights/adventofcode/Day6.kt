cb49dff98ae8050031a4e387d65861756b9d586a
Tommy <tommy.kvant@gmail.com>
2024-01-05 23:40:54 +0100

    Day 6 part 2

--- src/main/kotlin/com/arclights/adventofcode/Day6.kt
+++ src/main/kotlin/com/arclights/adventofcode/Day6.kt
@@ -1,11 +1,13 @@
 package com.arclights.adventofcode
 
 import com.arclights.adventofcode.Day6.part1
+import com.arclights.adventofcode.Day6.part2
 import kotlin.math.ceil
 import kotlin.math.sqrt
 
 fun main() {
     part1()
+    part2()
 }
 
 object Day6 {
@@ -13,22 +15,36 @@ object Day6 {
         val input = parse("Day6.txt")
         val result = input
             .map { (time, record) -> getNbrOfWaysToBeatRecord(time, record) }
-            .reduce(Int::times)
+            .reduce(Long::times)
         println(result)
     }
 
-    private fun getNbrOfWaysToBeatRecord(time: Int, record: Int) = (upperBound(time, record) - lowerBound(time, record))
-        .toInt()
+    fun part2() {
+        val (time, record) = parseWithoutKerning("Day6.txt")
+        val result = getNbrOfWaysToBeatRecord(time, record)
+        println(result)
+    }
 
-    private fun upperBound(time: Int, record: Int) = ceil(((time + sqrt((time * time - 4 * record).toDouble())) / 2))
+    private fun getNbrOfWaysToBeatRecord(time: Long, record: Long) =
+        (upperBound(time, record) - lowerBound(time, record))
 
-    private fun lowerBound(time: Int, record: Int) =
-        ((time - sqrt((time * time - 4 * record).toDouble())) / 2).toInt() + 1
+    private fun upperBound(time: Long, record: Long) =
+        ceil(((time + sqrt((time * time - 4 * record).toDouble())) / 2)).toLong()
+
+    private fun lowerBound(time: Long, record: Long) =
+        ((time - sqrt((time * time - 4 * record).toDouble())) / 2).toLong() + 1
 
     private fun parse(file: String) = read(file)
         .let { lines ->
-            val times = lines[0].split(":")[1].trim().split("\\s+".toRegex()).map { it.toInt() }
-            val distances = lines[1].split(":")[1].trim().split("\\s+".toRegex()).map { it.toInt() }
+            val times = lines[0].split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
+            val distances = lines[1].split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
             times.zip(distances)
+        }
+
+    private fun parseWithoutKerning(file: String) = read(file)
+        .let { lines ->
+            val time = lines[0].split(":")[1].filterNot { it == ' ' }.toLong()
+            val distance = lines[1].split(":")[1].filterNot { it == ' ' }.toLong()
+            time to distance
         }
 }
